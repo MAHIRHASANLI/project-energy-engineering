17d8fe330900045ac752241522b4337bb7637d17
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2023-11-28 01:08:45 +0400

    .

--- js/news.js
+++ js/news.js
@@ -9,22 +9,23 @@ var newWidthInPx = Math.floor(newWidth) + 'px';
 const newsCards = document.querySelector(".newsCards")
 const newsContainer = document.querySelector(".newsContainer")
 
-const rightBtn =document.getElementById("right")
+const rightBtn = document.getElementById("right")
 
-const leftBtn =document.getElementById("left")
+const leftBtn = document.getElementById("left")
 
 
 newsContainer.style.width = newsCardWidth + "px"
 newsCards.style.width = newsCardWidth + "px"
 
 let newsObjects = [
-   { title: "Xəbər 1", text: "This is the text for news 1.", img: "./images/technology-trend-freepik-1647963838.jpg", date:"10/06/2023" },
-   { title: "Xəbər 2", text: "This is the text for news 2.", img: "./images/Brad-Blog-Aus-Header-scaled.jpg", date:"10/06/2023" },
-   { title: "Xəbər 3", text: "This is the text for news 3.", img: "./images/Tech_4x3_608.jpg", date:"8/06/2023" },
-   { title: "Xəbər 4", text: "This is the text for news 4.", img: "./images/images.jpg", date:"10/06/2023" },
-   { title: "Xəbər 5", text: "This is the text for news 5.", img: "./images/download.jpg", date:"9/06/2023" },
-   { title: "Xəbər 6", text: "This is the text for news 6.", img: "./images/Surveillance-State_QBS_Drones-technology-and-apps_Featured.webp", date:"10/06/2023" },
+  { title: "Xəbər 1", text: "This is the text for news 1.", img: "./images/technology-trend-freepik-1647963838.jpg", date: "10/06/2023" },
+  { title: "Xəbər 2", text: "This is the text for news 2.", img: "./images/Brad-Blog-Aus-Header-scaled.jpg", date: "10/06/2023" },
+  { title: "Xəbər 3", text: "This is the text for news 3.", img: "./images/Tech_4x3_608.jpg", date: "8/06/2023" },
+  { title: "Xəbər 4", text: "This is the text for news 4.", img: "./images/images.jpg", date: "10/06/2023" },
+  { title: "Xəbər 5", text: "This is the text for news 5.", img: "./images/download.jpg", date: "9/06/2023" },
+  { title: "Xəbər 6", text: "This is the text for news 6.", img: "./images/Surveillance-State_QBS_Drones-technology-and-apps_Featured.webp", date: "10/06/2023" },
 ];
+
 newsObjects.sort((a, b) => {
   const dateA = new Date(a.date);
   const dateB = new Date(b.date);
@@ -35,45 +36,46 @@ newsObjects.reverse();
 
 
 newsObjects.forEach((element) => {
-   const newsDiv = document.createElement("div");
-   newsDiv.className = "news";
+  console.log(element);
+  const newsDiv = document.createElement("div");
+  newsDiv.className = "news";
 
-   newsDiv.style.minWidth = newWidthInPx
+  newsDiv.style.minWidth = newWidthInPx
 
-   const newsImgDiv = document.createElement("img");
-   newsImgDiv.className = "newsImg";
-   newsImgDiv.src = element.img;
- 
-   const newsTextDiv = document.createElement("div");
-   newsTextDiv.className = "newsText";
+  const newsImgDiv = document.createElement("img");
+  newsImgDiv.className = "newsImg";
+  newsImgDiv.src = element.img;
 
-   const paragraph = document.createElement("p");
-   paragraph.textContent = element.title; // Use the title from the newsObjects array
+  const newsTextDiv = document.createElement("div");
+  newsTextDiv.className = "newsText";
 
-   // Append the paragraph to the newsTextDiv
-   newsTextDiv.appendChild(paragraph);
+  const paragraph = document.createElement("p");
+  paragraph.textContent = element.title; // Use the title from the newsObjects array
 
-   
+  // Append the paragraph to the newsTextDiv
+  newsTextDiv.appendChild(paragraph);
+
+
   const newsDateDiv = document.createElement("div");
   newsDateDiv.className = "newsDate";
   newsDateDiv.textContent = "Date: " + element.date;
 
-   // Append the newsImgDiv and newsTextDiv to the main newsDiv
-   newsDiv.appendChild(newsImgDiv);
-   newsDiv.appendChild(newsTextDiv);
-   newsDiv.appendChild(newsDateDiv);
+  // Append the newsImgDiv and newsTextDiv to the main newsDiv
+  newsDiv.appendChild(newsImgDiv);
+  newsDiv.appendChild(newsTextDiv);
+  newsDiv.appendChild(newsDateDiv);
 
-   // Append the main newsDiv to the body of the document
-   newsCards.appendChild(newsDiv);
-   // Attach the click event handler to the newsCards container
-   
+  // Append the main newsDiv to the body of the document
+  newsCards.appendChild(newsDiv);
+  // Attach the click event handler to the newsCards container
+
 
 })
 
 newsCards.addEventListener("click", (event) => {
   const clickedNewsDiv = event.target.closest(".news");
   if (!clickedNewsDiv) {
-     return; 
+    return;
   }
 
   const title = clickedNewsDiv.querySelector(".newsText p").textContent;
@@ -83,8 +85,8 @@ newsCards.addEventListener("click", (event) => {
 
   const newsDetails = document.createElement("div");
   newsDetails.classList.add("details");
-  
-  
+
+
   const newsDetailsContainer = document.createElement("div");
   newsDetailsContainer.classList.add("newsDetailsContainer");
 
@@ -92,7 +94,7 @@ newsCards.addEventListener("click", (event) => {
   cancel.innerHTML = "X";
 
   const newsTextCont = document.createElement("div");
-   newsTextCont.className = "newsTextCont";
+  newsTextCont.className = "newsTextCont";
 
   const detailsTitle = document.createElement("h2");
   detailsTitle.textContent = title;
@@ -112,7 +114,7 @@ newsCards.addEventListener("click", (event) => {
   newsTextCont.appendChild(detailsTitle);
   newsTextCont.appendChild(detailsText);
   newsTextCont.appendChild(detailsDate); // Add the date to the detail page
-newsDetailsContainer.appendChild(newsTextCont)
+  newsDetailsContainer.appendChild(newsTextCont)
   newsDetails.appendChild(newsDetailsContainer);
   document.body.appendChild(newsDetails);
 
@@ -121,9 +123,9 @@ newsDetailsContainer.appendChild(newsTextCont)
   htmlElement.style.scrollbarWidth = "0"; // Firefox
 
   cancel.addEventListener("click", () => {
-     if (newsDetails) {
-        newsDetails.remove();
-     }
+    if (newsDetails) {
+      newsDetails.remove();
+    }
   });
 });
 
@@ -143,7 +145,7 @@ function scrollRight() {
   const isVisible = elementRect.left < window.innerWidth && elementRect.right > 0;
 
   if (isVisible) {
-    const scrollAmount = newsCards.scrollLeft + newWidth; 
+    const scrollAmount = newsCards.scrollLeft + newWidth;
 
     if (scrollAmount >= newsCards.scrollWidth - newsCards.clientWidth) {
       newsCards.scrollTo({
@@ -165,7 +167,7 @@ function scrollLeft() {
   const isVisible = elementRect.right < window.innerWidth && elementRect.left > 0;
 
   if (isVisible) {
-    const scrollAmount = newsCards.scrollLeft - newWidth; 
+    const scrollAmount = newsCards.scrollLeft - newWidth;
 
     if (scrollAmount >= newsCards.scrollWidth - newsCards.clientWidth) {
       newsCards.scrollTo({
@@ -184,10 +186,10 @@ function scrollLeft() {
 
 
 
-rightBtn.addEventListener("click",()=>{
+rightBtn.addEventListener("click", () => {
   scrollRight()
 })
-leftBtn.addEventListener("click",()=>{
+leftBtn.addEventListener("click", () => {
   scrollLeft()
 })
 
@@ -195,17 +197,17 @@ const observer = new IntersectionObserver((entries) => {
   const isVisible = entries[0].isIntersecting;
   if (isVisible) {
     clearInterval(scrollInterval);
-    scrollInterval = setInterval(scrollRight, 5000); 
+    scrollInterval = setInterval(scrollRight, 5000);
   }
 });
 
 newsCards.addEventListener('scroll', () => {
-  clearInterval(scrollInterval); 
-  scrollInterval = setInterval(scrollRight, 5000); 
+  clearInterval(scrollInterval);
+  scrollInterval = setInterval(scrollRight, 5000);
 });
 
 newsCards.addEventListener('click', () => {
-  clearInterval(scrollInterval); 
+  clearInterval(scrollInterval);
   scrollInterval = setInterval(scrollRight, 5000);
 });
 
@@ -213,4 +215,4 @@ observer.observe(newsCards);
 
 const maxIterations = 10;
 let iterations = 0;
-   
+
